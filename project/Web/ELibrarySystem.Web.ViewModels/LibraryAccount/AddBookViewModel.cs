edb0f43d4f3b2bd2073207eea6750743a542e44d
toncheva_v <toncheva_v@abv.bg>
2019-10-08 13:04:12 +0300

    some changes

--- project/Web/ELibrarySystem.Web.ViewModels/LibraryAccount/AddBookViewModel.cs
+++ project/Web/ELibrarySystem.Web.ViewModels/LibraryAccount/AddBookViewModel.cs
@@ -7,7 +7,6 @@
 
     public class AddBookViewModel
     {
-
         public AddBookViewModel()
         {
             this.BookId = " ";
---
+++ project/Web/ELibrarySystem.Web.ViewModels/UserAccount/GetBookViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELibrarySystem.Web.ViewModels.UserAccount
+{
+    public class GetBookViewModel
+    {
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+
+        public string GenreName { get; set; }
+
+        public string GenreId { get; set; }
+
+        public string BookId { get; set; }
+    }
+}
